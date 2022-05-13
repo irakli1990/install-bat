9fcdb6371a00a421f18fd78192914d04e114c396
Irakli <iraklikardawa@gmail.com>
2022-05-13 10:05:58 +0200

    CHG: native

--- install.bat
+++ install.bat
@@ -1,1 +1,5 @@
+runas /user:Administrator
 
+echo ala ma kota
+
+powershell -Command "Invoke-WebRequest https://github.com/irakli1990/install-bat/blob/master/com.infolab.reader.zip"
---
+++ native/com.infolab.reader.bat
@@ -0,0 +1,2 @@
+@echo off
+chrome_reader.exe
